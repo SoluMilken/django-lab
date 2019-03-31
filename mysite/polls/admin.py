e035dcb9b45d91a06ce472a193c0488052c6fd26
Solumilken <s916526000@gmail.com>
2019-03-31 18:16:17 +0800

    fitted flake8

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,7 @@
-# from django.contrib import admin
+from django.contrib import admin
 
 from .models import Question
 
 
-admin.site.register(Question)  # tell the admin that Question objects have an admin interface
+# tell the admin that Question objects have an admin interface
+admin.site.register(Question)
