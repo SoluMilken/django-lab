9effcbc4f5d6f9ee7ccba9603edf7710f740eea9
Solumilken <s916526000@gmail.com>
2019-04-10 18:17:40 +0800

    view has html template

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,13 +1,17 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
 from .models import Question
 
 
 def index(request):
     # query the latest five questions from DB
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    # use template from polls/templates/polls/index.html
+    template = loader.get_template('polls/index.html')
+    context = {'latest_question_list': latest_question_list}
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, question_id):
