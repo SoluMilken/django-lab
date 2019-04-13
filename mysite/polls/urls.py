06648c899fb765791303a4e78aa356871a4a31b1
Solumilken <s916526000@gmail.com>
2019-04-13 16:30:30 +0800

    fix_bug: extra space

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -14,13 +14,13 @@ urlpatterns = [
     ),  # run def details in views.py
     # ex: /polls/5/results/
     path(
-        '<int: question_id>/results/',
+        '<int:question_id>/results/',
         views.results,
         name='results',
     ),  # run def results in views.py
     # ex: /polls/5/vote/
     path(
-        '<int: question_id>/vote/',
+        '<int:question_id>/vote/',
         views.vote,
         name='vote',
     ),  # run def results in views.py
